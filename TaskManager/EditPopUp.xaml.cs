29f53374014f6bf9b62f5a7e2b6b69cc265d1ce1
Oleksandr Rudyi <calllok.rud@gmail.com>
2018-12-16 21:04:13 +0200

    Initial commit

---
+++ TaskManager/EditPopUp.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Interaction logic for EditPopUp.xaml
+    /// </summary>
+    public partial class EditPopUp : Window
+    {
+        private string tempdesc;
+
+        public EditPopUp(ToDoItem item)
+        {
+            InitializeComponent();
+            DataContext = item;
+            tempdesc = item.Description;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bool result = DialogResult ?? false;
+            if (!result)
+                (DataContext as ToDoItem).Description = tempdesc;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Worklist worklist = new Worklist();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = worklist;
+        }
+
+        private void EditMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                EditPopUp editPopUp = new EditPopUp(lvToDo.SelectedItem as ToDoItem);
+                editPopUp.Owner = this;
+                editPopUp.ShowDialog();
+            }
+        }
+
+        private void MarkAsDone(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                MessageBoxResult done = MessageBox.Show("Do you want to mark this task as done?", "Mark as Done", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (done == MessageBoxResult.Yes)
+                {
+                    (lvToDo.SelectedItem as ToDoItem).DoneDateTime = DateTime.Now.ToString("yyyy-MM-ddThh:mm:ss.ms");
+                }
+                lvToDo.Items.Refresh();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                MessageBoxResult del = MessageBox.Show("Do you want to delete selected task?", "Delete Task", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (del == MessageBoxResult.Yes)
+                {
+                    worklist.DeleteItem(lvToDo.SelectedItem as ToDoItem);
+                }
+                lvToDo.Items.Refresh();
+            }
+        }
+
+        private void lvToDo_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MarkAsDone(sender, e);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtItemDesc.Text.Trim().Equals(string.Empty))
+            {
+                worklist.Additem(txtItemDesc.Text.Trim());
+            }
+            txtItemDesc.Text = "";
+            lvToDo.Items.Refresh();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            worklist.Save();
+        }
+    }
+}
